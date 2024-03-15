a9a45c4ed9f4bfaac0ea4ef3aa529c6dcfc5873f
saurabh <kapanarinsal6@gmail.com>
2024-03-15 23:50:53 +0530

    updated shit

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-let num = 3; age=5; str = "5"
+/*let num = 3; age=5; str = "5"
 add = num+age+str // this thing doesnt' work in python , you can't add int with string
-console.log(add)
-alert(add)  
+console.log(add)  
+$ = 234 
+$$ = "string randowm" // dollar sign can be used as var name ?
+//var is for global scope 
+//let is for block scope 
+/* variable can't be redeclared using let 
+but in a block let redeclaration works*/
+
+
+const pi = 3.14; //defines a constant reference not a constant value 
+const array = ["shit", "some more", "little more"]
+console.log(array[0]) //a js array is like python list
