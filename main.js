6a8004aad0e8b0941417e05c3017f334cec30ca7
saurabh <kapanarinsal6@gmail.com>
2024-03-15 16:48:15 +0530

    added main.js already worked upon

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "edited";
+document.write("testing");
+window.alert("hi this is windows.alert"); // window object is global scope object, alert is a method, can be used without specifying window
+windows.print(); //prints the contents of current page
+document.getElementById("sleep") = "polyphasic";
+
