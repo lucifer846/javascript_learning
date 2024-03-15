38055fc0dc67e67378d1239b3dc9a0c22df59d6b
saurabh <kapanarinsal6@gmail.com>
2024-03-16 00:14:34 +0530

    conpare strings

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-/*let num = 3; age=5; str = "5"
-add = num+age+str // this thing doesnt' work in python , you can't add int with string
-console.log(add)  
-$ = 234 
-$$ = "string randowm" // dollar sign can be used as var name ?
-//var is for global scope 
-//let is for block scope 
-/* variable can't be redeclared using let 
-but in a block let redeclaration works*/
-
-
-const pi = 3.14; //defines a constant reference not a constant value 
-const object = {name:"raju", age:34, car:"maruti"} // this js obj looks like python dict??
-console.log(object.name) // but it doesn't work like python dict
-object.age = 12 // using const for object declaring but still can change properties of obj
-console.log(object)// but still can't reassgn this shit
+let a,b;
+a = "A"
+b = "B"
+console.log(a>b) // strings can also be compared in python (alphabatically)
+// this prints false
