6dcc0952611c9df4e1a77989af4260112abcbeff
saurabh <kapanarinsal6@gmail.com>
2024-03-15 22:16:09 +0530

    updated readme

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-document.getElementById("demo").innerHTML = "edited";
-document.write("testing");
-window.alert("this is windows.alert"); // window object is global scope object, alert is a method, can be used without specifying window
-windows.print(); //prints the contents of current page
-document.getElementById("sleep") = "polyphasic";
-let x, y, z;
-x = 'it'
-y = "some more shit"
-let a,b,c;
-a="line";b='same line';c='same line'; 
-/*this is
-a multiline shit 
-that wont be executed*/
-$abc='this is allowed',_69=69 // variables can start with $ or _ but not with number
-//like python js is case sensitive
-// var keyword is old not used nowdays , let is used now
+let num = 3; age=5; str = "5"
+add = num+age+str // this thing doesnt' work in python , you can't add int with string
+console.log(add)
+alert(add)  
