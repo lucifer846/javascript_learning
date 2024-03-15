61adaf98328fb12bdab65fb224528e702bd237ad
saurabh <kapanarinsal6@gmail.com>
2024-03-15 21:04:37 +0530

    timepass

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ document.getElementById("demo").innerHTML = "edited";
 document.write("testing");
 window.alert("this is windows.alert"); // window object is global scope object, alert is a method, can be used without specifying window
 windows.print(); //prints the contents of current page
-document.getElementById("spe") = "polyphasic";
+document.getElementById("sleep") = "polyphasic";
 let x, y, z;
 x = 'it'
-y = "some morehit"
+y = "some more shit"
 let a,b,c;
 a="line";b='same line';c='same line'; 
 /*this is
 a multiline shit 
 that wont be executed*/
 $abc='this is allowed',_69=69 // variables can start with $ or _ but not with number
-
+//like python js is case sensitive
+// var keyword is old not used nowdays , let is used now
