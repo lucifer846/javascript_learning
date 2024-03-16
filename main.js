058062eb5a9dc983f858b9f241219d40e6a7cfcf
saurabh <kapanarinsal6@gmail.com>
2024-03-16 16:23:31 +0530

    updated

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-let a,b;
-a = "A"
-b = "B"
-console.log(a>b) // strings can also be compared in python (alphabatically)
-// this prints false
+// && is logical and operator
+// || is logical or operator
+// ! is logical not operator
+// ** is same as Math.pow(x,3) function
+
+function display(a, b) { // function naming is same as variable
+    return a*b
+}
+
+obj = {
+    age:23,
+    car:"maruti",
+    show: function() {
+        console.log("shown")
+        console.log(this.age+" is the age")
+        console.log(this.car+" is the car")
+    }
+}
+obj.show()
+// there aer a lot of html events for which js can respond
