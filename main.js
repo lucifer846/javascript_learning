79669076cd36c151aa20dd6910810e3512166993
saurabh <kapanarinsal6@gmail.com>
2024-04-21 18:06:26 +0530

    added some

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
-// const req = require("./mouse")
-// console.log(req)
-// console.log      
+/*const req = require("./mouse")
+console.log(req.car)
+const mouse = ["Banana", "language", "mango"]*/
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+//console.log(fruits.indexOf("Orange"))
+num = [16,4,29,9,25];
 
 
-cars = ["maruti", "suzuki", "toyota", "fortuner"]
-let text0 = "<ul>"
-for (let i = 0;i<cars.length;i++) {
-    text0 += "<li>"+cars[i]+"</li>"
-}
-text0+="</ul>"
-console.log(text0)
+console.log(num.sort((a,b) => {return a-b}));//this is compare function that sorts numbers
+console.log(num.sort((a,b) => {return b-a}));
 
+console.log(num.sort(()=>{return 0.5 - Math.random()})); //maths.random() returns pseudorandom num b/w 0 to 1
 
-let text = "<ul>"
-
-cars = ["maruti", "suzuki", "toyota", "fortuner"]
-cars.forEach((element) => { // forEach method instead of looping
-    text+="<li>"+element+"</li>"
-});
-text+="</ul>"
-console.log(text)
-
-cars.forEach((i) => {
-    console.log(`print ${i}`)
-}) 
-
+for (let i = num.length -1; i > 0; i--) { // fisher yates shuffle 
+    let j = Math.floor(Math.random() * (i+1));
+    let k = num[i];
+    num[i] = num[j];
+    num[j] = k;
+  }
+console.log(num);
