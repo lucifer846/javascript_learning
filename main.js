eaac155ee522f2f03c5475d41f06bd9c500167dc
saurabh <kapanarinsal6@gmail.com>
2024-04-21 19:51:26 +0530

    forEach and map methods

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ const fruits = ["Apple", "Orange", "Apple", "Mango"];
 //console.log(fruits.indexOf("Orange"))
 num = [16,4,29,9,25];
 
+let text = "";
 
-console.log(num.sort((a,b) => {return a-b}));//this is compare function that sorts numbers
-console.log(num.sort((a,b) => {return b-a}));
+num.forEach(element => { // forEach function takes three arguments(value,index,array)
+    text += element + "<br>"            //but here only one could be given
+});
+console.log(text)
+let new_num = num.map(f);
 
-console.log(num.sort(()=>{return 0.5 - Math.random()})); //maths.random() returns pseudorandom num b/w 0 to 1
-
-for (let i = num.length -1; i > 0; i--) { // fisher yates shuffle 
-    let j = Math.floor(Math.random() * (i+1));
-    let k = num[i];
-    num[i] = num[j];
-    num[j] = k;
-  }
-console.log(num);
+function f(elmt) {
+    return elmt *2; 
+}
+console.log(new_num);
