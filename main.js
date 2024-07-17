3cf5d2396acca33c6ca18fa80fe0bf20f7f8fddc
saurabh <kapanarinsal6@gmail.com>
2024-07-17 18:10:43 +0530

    learned DOM js after aprox 50 days of no laptop

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
-const fruits = ["Apple", "Orange", "Apple", "Mango"];
-num = [16,4,29,9,25];
+/*const fruits = ["Apple", "Orange", "Apple", "Mango"];
 
-age = 17/"raju"
+let btn1 = document.querySelector("#tryit");
 
-if (isNaN(age)) {
-    adult = "input is not a number"
+btn1.onclick = () => {
+    alert("what's up Beachh")
 }
-else {
-    adult = (age>18)? "yes":"no";  // variable value based on condition
+
+let btn2 = document.querySelector("#endbutton");
+
+btn2.onclick = () => {
+    console.log("working")
 }
-console.log(adult)
+
+let div = document.querySelector('div')
+div.onmouseover = () => {console.log('you hovered over the div');}
+
+class Car {
+    constructor(color, model, year) {
+        this.color = color
+        this.model = model
+        this.year = year
+    }
+    checkscore() {
+        if (this.year>2010) {
+            console.log("Good Score")
+        }
+        else {
+            console.log("nah shove it up your arse")
+        }
+    }
+}d
+
+car = new Car('white', 's-class', '2019')
+car.checkscore()*/
+
+let btn = document.createElement("button");
+btn.innerText = "click me bitch"
+
+document.querySelector("#endbutton").after(btn);
+document.querySelector("#endbutton").remove();
+btn.onclick = () => {
+    location = "https://www.google.com"
+}
