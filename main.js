cb6cfc85e2cba0d4c17ddcdeb23c47c601710e77
saurabh <kapanarinsal6@gmail.com>
2024-11-17 10:02:11 +0530

    made some promises..... in javascript :)

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+// let prom = new Promise((resolve, reject) => {
+//     console.log("this is promise")
+// })
+
+
+
 function get_data(data_id, getnextdata) {
-    setTimeout(() => {
+    return new Promise((resolve,reject)=> {
+        setTimeout(() => {
         console.log("this is the data", data_id);
         if (getnextdata) {
             getnextdata();}
-    }, (2000));
-
+        resolve("success")
+    }, (4000));
 }
-//callback hell
-get_data(1, ()=>{
-    get_data(2, ()=>{
-        get_data(3)
-    });
-})
+)}
+
+// }
+// //callback hell
+// get_data(1, ()=>{
+//     get_data(2, ()=>{
+//         get_data(3)
+//     });
+// })
