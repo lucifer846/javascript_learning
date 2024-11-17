7c63788f7851222fec8b375d53db5a5453ef1070
saurabh <kapanarinsal6@gmail.com>
2024-11-17 10:29:10 +0530

    promise chaining ,,,, was thinking if this can be used for PROXY chaining programming ??? i am not quite there yet

--- main.js
+++ main.js
@@ -10,10 +10,18 @@ function get_data(data_id, getnextdata) {
         console.log("this is the data", data_id);
         if (getnextdata) {
             getnextdata();}
-        resolve("success")
-    }, (4000));
+        resolve("this is the data")
+    }, (2000));
 }
 )}
+console.log("fetching data 1...")
+let prom1 = get_data(123)
+prom1.then((res)=>{
+    console.log("fetching data 2...")
+    let prom2 = get_data(535)
+    prom2.then(res)
+
+})
 
 // }
 // //callback hell
