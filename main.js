6f812551534d9335df4d77c9baebd53a9272a31b
saurabh <kapanarinsal6@gmail.com>
2024-11-17 20:14:08 +0530

    getting cat facts using api by promise chaining or async await

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
-// let prom = new Promise((resolve, reject) => {
-//     console.log("this is promise")
-// })
+const url = "https://cat-fact.herokuapp.com/facts"
+const facePara = document.querySelector("#catfact")
+const btncatfact = document.querySelector("#tellcatfact")
 
 
-(async function(){
-    await get_data(1)
-    await get_data(2)
-    await get_data(3)
-    await get_data(4)
-    await get_data(5)
-})();
-
-
-function get_data(data_id) {
-    return new Promise((resolve,reject)=> {
-        console.log(`fetching data ...${data_id}`)
-        setTimeout(() => {
-
-        console.log("data", data_id);
-        resolve("this shit is resolved")
-    }, (2000));
-}
-)}
-
-
-//  // promice chaining
-// get_data(1).then((res) => {
-//         return get_data(2);
-//     })
-//     .then((res)=>{
-//         console.log(res)
-//     })
+// const getfacts = async () => {
+//     let response = await fetch(url);
+//     //console.log(response); //JSON format
+//     let js_obj = await response.json();
+//     facePara.innerHTML = js_obj[0].text
+    
 // }
-// //callback hell
-// get_data(1, ()=>{
-//     get_data(2, ()=>{
-//         get_data(3)
-//     });
-// })
 
+function getfacts(){
+    fetch(url).then((res)=> {
+        return res.json()
+    }).then((data)=>{
+        facePara.innerHTML = data[0].text
+    })
+}
 
+btncatfact.addEventListener("click", getfacts)
