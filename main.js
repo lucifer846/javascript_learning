3ded3df179171eb4cd3ea73d3e34198e60369e85
saurabh <kapanarinsal6@gmail.com>
2024-11-17 11:17:28 +0530

    did the same thing with async await , its easier than promise chaining and doesn't crease callback hell

--- main.js
+++ main.js
@@ -3,33 +3,28 @@
 // })
 
 
-
-function api(){
-    return new Promise((resolve, reject)=>{
-       setTimeout(() => {
-        console.log("weather data");
-        resolve(200)
-       }, (2000));
-
-    })
+async function get_all_data(){
+    await get_data(1)
+    await get_data(2)
+    await get_data(3)
+    await get_data(4)
+    await get_data(5)
 }
 
-async function get_weather_data(){
-    await api()
-    await api()
+get_all_data()
+
+function get_data(data_id) {
+    return new Promise((resolve,reject)=> {
+        console.log(`fetching data ...${data_id}`)
+        setTimeout(() => {
+
+        console.log("data", data_id);
+        resolve("this shit is resolved")
+    }, (2000));
 }
+)}
 
-get_weather_data()
-// function get_data(data_id) {
-//     return new Promise((resolve,reject)=> {
-//         console.log(`fetching data ...${data_id}`)
-//         setTimeout(() => {
 
-//         console.log("this is the data", data_id);
-//         resolve("this shit is resolved")
-//     }, (2000));
-// }
-// )}
 //  // promice chaining
 // get_data(1).then((res) => {
 //         return get_data(2);
