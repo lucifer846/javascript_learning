7efeb1b6b8f0d362ab11978e7044f6bbc580ca46
saurabh <kapanarinsal6@gmail.com>
2024-11-18 14:13:46 +0530

    mkdir and rmdir and existsSync() function for checking if file exists also unlink method for deleting a file

--- files.js
+++ files.js
@@ -1,5 +1,6 @@
 const { error } = require('console');
 const fs = require('fs');
+const { fileURLToPath } = require('url');
 
 //reading files
 // fs.readFile('./blog.txt', (err, data)=>{
@@ -10,8 +11,33 @@ const fs = require('fs');
 //     }
 // })
 
-fs.writeFile('./blog.txt', "this file is rewritten", ()=>{
-    console.log("file was written")
-})
+// fs.writeFile('./blog.txt', "this file is rewritten", ()=>{
+//     console.log("file was written")
+// })
 
-console.log("last line")
+// console.log("last line")
+
+filepath = './assets'
+
+if (!fs.existsSync(filepath)){
+    fs.mkdir((filepath), (err)=>{
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("file created")
+      }
+    })
+}
+else {
+    fs.rmdir(filepath, (err)=>{
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("file deleted")
+        }
+    })
+}
+
+fs.unlink('./blog.txt', ()=>{
+    console.log("file is deleted")
+})
