378d81c5a0149acc25bd009ed2fcef5983412ade
saurabh <kapanarinsal6@gmail.com>
2024-11-18 11:51:10 +0530

    require and export i guess

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-const url = "https://cat-fact.herokuapp.com/facts"
-const facePara = document.querySelector("#catfact")
-const btncatfact = document.querySelector("#tellcatfact")
+const {fruits, minerals} = require("./module")
 
+console.log(fruits, minerals)
 
-// const getfacts = async () => {
-//     let response = await fetch(url);
-//     //console.log(response); //JSON format
-//     let js_obj = await response.json();
-//     facePara.innerHTML = js_obj[0].text
-    
-// }
-
-function getfacts(){
-    fetch(url).then((res)=> {
-        return res.json()
-    }).then((data)=>{
-        facePara.innerHTML = data[0].text
-    })
-}
-
-btncatfact.addEventListener("click", getfacts)
+const os = require("os")
+console.log(os.platform(), os.homedir())
