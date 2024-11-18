1a672dc85949d280765ce971793caaa6f21fcdac
saurabh <kapanarinsal6@gmail.com>
2024-11-18 13:56:21 +0530

    node js fs.writeFile if the file doesnt exist it creates a new file and writes in it

--- files.js
+++ files.js
@@ -2,11 +2,16 @@ const { error } = require('console');
 const fs = require('fs');
 
 //reading files
-fs.readFile('./blog.txt', (err, data)=>{
-    if (err){
-        console.log(err)
-    } else {
-        console.log(data.toString())
-    }
+// fs.readFile('./blog.txt', (err, data)=>{
+//     if (err){
+//         console.log(err)
+//     } else {
+//         console.log(data.toString())
+//     }
+// })
+
+fs.writeFile('./blog.txt', "this file is rewritten", ()=>{
+    console.log("file was written")
 })
+
 console.log("last line")
